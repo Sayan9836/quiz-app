5206c9377c532517c6e7c37756424585b7cd7a37
sayan <Sayan9836>
2022-09-03 22:29:39 +0530

    quiz-app

--- src/components/Data.js
+++ src/components/Data.js
@@ -46,6 +46,50 @@ const data = [
     },
     {
         id: 3,
+        question: "Which of these sports requires you to shout out a word loudly during play?",
+        answers: [
+            {
+                text: "Ludo",
+                correct: false,
+            },
+            {
+                text: "Kho-kho",
+                correct: true,
+            },
+            {
+                text: "Playing-cards",
+                correct: false,
+            },
+            {
+                text: "Chess",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 4,
+        question: "Which of these spices is the smallest in size?ovie?",
+        answers: [
+            {
+                text: "Ajwain",
+                correct: true,
+            },
+            {
+                text: "Jeera",
+                correct: false,
+            },
+            {
+                text: "Saunf",
+                correct: false,
+            },
+            {
+                text: "Methi Seeds",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 5,
         question: "Who played the character of harry potter in movie?",
         answers: [
             {
@@ -63,6 +107,138 @@ const data = [
             {
                 text: "Daniel Red Cliff",
                 correct: true,
+            },
+        ],
+    },
+    {
+        id: 6,
+        question: "Which battle in 1757 marked the beginning of British occupation in India?",
+        answers: [
+            {
+                text: "Plassey",
+                correct: true,
+            },
+            {
+                text: "Assaye",
+                correct: false,
+            },
+            {
+                text: "Buxar",
+                correct: false,
+            },
+            {
+                text: "Cuddalore",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 7,
+        question: " Which is the second most spoken language of Nepal?",
+        answers: [
+            {
+                text: "Bajjika",
+                correct: false,
+            },
+            {
+                text: "Nepali",
+                correct: false,
+            },
+            {
+                text: "Maithili",
+                correct: true,
+            },
+            {
+                text: "BhojpuriView",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 8,
+        question: "In which of these two sports is the term ‘free hit’ used?",
+        answers: [
+            {
+                text: "Football, Squash",
+                correct: false,
+            },
+            {
+                text: "Badminton, Tennis",
+                correct: false,
+            },
+            {
+                text: " Badminton, Cricket",
+                correct: false,
+            },
+            {
+                text: "Hockey, Cricket",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 9,
+        question: "Which of these medical conditions is most likely to cause dehydration?",
+        answers: [
+            {
+                text: "Malaria",
+                correct: false,
+            },
+            {
+                text: "Tetanus",
+                correct: false,
+            },
+            {
+                text: "Diarrhoea",
+                correct: true,
+            },
+            {
+                text: "Beriberi",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 10,
+        question: "What is gulab jamun a type of?",
+        answers: [
+            {
+                text: "A flower",
+                correct: false,
+            },
+            {
+                text: "A fruit",
+                correct: false,
+            },
+            {
+                text: "A tree",
+                correct: false,
+            },
+            {
+                text: "A sweet",
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: 11,
+        question: "In which of these sports are the terms reverse swing and reverse sweep used?",
+        answers: [
+            {
+                text: "Football",
+                correct: false,
+            },
+            {
+                text: "Cricket",
+                correct: true,
+            },
+            {
+                text: "Badminton",
+                correct: false,
+            },
+            {
+                text: "Tennis",
+                correct: false,
             },
         ],
     },
